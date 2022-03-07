38a155548bc5aecf283b75a12e5eed1bf0744431
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-03-07 10:23:11 -0500

    upload files

---
+++ index.js
@@ -0,0 +1,85 @@
+/* jshint esversion:8*/
+
+const colorPicker = {
+  type: "Color",
+  isEdit: true,
+  run: (name, initialValue, attributes, htmlClass) => {
+    htmlClass = "color" + (Math.floor(Math.random() * 10000) + 1);
+    return `
+      <div class="colorPickerContainer ${htmlClass}"></div>
+      <script>
+        window.addEventListener('DOMContentLoaded', (event) => {
+          createColorPicker("${name}", "${initialValue}", undefined, "${htmlClass}");
+        });
+      </script>
+    `;
+  },
+};
+
+const show_text = {
+  type: "Color",
+  isEdit: false,
+  run: (value) => {
+    return `${value}`;
+  },
+};
+
+const show_text_swatch = {
+  type: "Color",
+  isEdit: false,
+  run: (value) => {
+    return `
+      <div class="showSwatch" style="
+        background-color: ${value};
+        height: 1rem;
+        width: 1rem;
+        display: inline-block;
+        border-radius: 3px;
+        box-shadow: inset 0 0 1px 1px rgba(0,0,0,.1);
+        margin-bottom: 0.3rem;
+        vertical-align: middle;
+      "></div>
+      ${value}
+    `;
+  },
+};
+
+const show_swatch = {
+  type: "Color",
+  isEdit: false,
+  run: (value) => {
+    return `
+      <div class="showSwatch" style="
+        background-color: ${value};
+        height: 1rem;
+        width: 1rem;
+        display: inline-block;
+        border-radius: 3px;
+        box-shadow: inset 0 0 1px 1px rgba(0,0,0,.1);
+        margin-bottom: 0.3rem;
+        vertical-align: middle;
+      "></div>
+    `;
+  },
+};
+
+const headers = [
+  {
+    script: "/plugins/public/saltcorn-colors/main.js",
+  },
+  {
+    css: "/plugins/public/saltcorn-colors/main.css",
+  },
+];
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  headers,
+  plugin_name: "saltcorn-colors",
+  fieldviews: {
+    colorPicker,
+    show_text,
+    show_text_swatch,
+    show_swatch,
+  },
+};
